fc0d01d545aeec4722e4ac10ac16d3d5b35bfdbd
ankit <khadka.ankit2004@gmail.com>
2025-10-05 01:03:41 +0545

    Making the user profile website , adding the default profile pic

--- Site/settings.py
+++ Site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,6 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [ 
     BASE_DIR / 'static',
     ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')  # uploaded file located at the file system
+MEDIA_URL = '/media/' 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
