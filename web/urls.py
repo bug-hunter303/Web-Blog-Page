36ee9993b39a6cf72ae4da231da04a2144bc053f
ankit <khadka.ankit2004@gmail.com>
2025-10-05 07:55:31 +0545

    Making the create html and detail of a sinlge particular post

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from web import views
+from . import views
 
 urlpatterns = [
     path('', views.home, name = 'web-home'), 
     path('about/',views.about, name='web-about'),
+    path('detail/<int:pk>/',views.detail,name='web-detail'),
+    path('create/',views.create,name='web-create'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,12 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect , get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from web.forms import PostForm
+from django.contrib import messages
+
 
 def home(request):
     context = {
-        'posts': Post.objects.all()
+        'posts': Post.objects.all().order_by("-date_posted")
     }
     return render(request, 'home.html', context)
 
 def about(request):
     return render(request, 'about.html',{'title': 'About'})
+
+# class PostListView(ListView):
+
+    model = Post
+    context_object_name = 'posts'
+
+def detail(request , pk):
+    context = {
+        'post': Post.objects.get(pk = pk) # or get_object_or_404
+    }
+
+    return render(request , 'detail.html',context)
+
+def create(request):
+    
+    if request.method == "GET":
+        form = PostForm()
+    else:
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            messages.success(request, 'The post has been created successfully')
+            return redirect('web-home')
+    
+    return render(
+        request,
+        'create.html',
+        {'form': form}
+    )
